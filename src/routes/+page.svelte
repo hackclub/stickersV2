4aa434d6f8f56fb600fe916974dfc26e49c20267
maxstellar <93678379+maxstellar@users.noreply.github.com>
2026-04-12 23:21:31 -0700

    feat: add form, i guess

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,8 @@
     <div class="hero-title">
         <h1>stickers</h1>
         <h2>hack club</h2>
+        <form>
+            <input id="email" placeholder="your@email.com" type="email" required>
+        </form>
     </div>
 </div>
