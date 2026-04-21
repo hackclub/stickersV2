3168203eb4b9359239f43f418c722db21a05e49d
maxstellar <93678379+maxstellar@users.noreply.github.com>
2026-04-21 16:29:03 -0700

    feat: add new favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={favicon} type="image/x-icon" />
 </svelte:head>
 
 {@render children()}
