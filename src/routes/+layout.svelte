0e4795debd11454463e9ca8cb946f72cd8b64c7c
maxstellar <93678379+maxstellar@users.noreply.github.com>
2026-04-21 19:47:54 -0700

    feat: move favicon back to static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.ico';
-
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} type="image/x-icon" />
-</svelte:head>
 
 {@render children()}
