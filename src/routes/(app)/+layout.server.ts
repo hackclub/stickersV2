ab7b5488141b0a5977a7eec31de6ad8e66f8d1c1
maxstellar <93678379+maxstellar@users.noreply.github.com>
2026-05-04 21:38:56 -0700

    feat: bounce non-onboarded users from dashboard back to onboarding (created exception for admins to make development easier)

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -4,5 +4,6 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	if (!locals.user) throw redirect(302, '/');
+	if (!locals.user.onboarded_at && !isAdmin(locals.user.hca_id)) throw redirect(302, '/onboarding');
 	return { user: locals.user, isAdmin: isAdmin(locals.user.hca_id) };
 };
--- src/routes/onboarding/+layout.server.ts
+++ src/routes/onboarding/+layout.server.ts
@@ -1,8 +1,9 @@
 import { redirect } from '@sveltejs/kit';
+import { isAdmin } from '$lib/server/admin';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	if (!locals.user) throw redirect(302, '/');
-	if (locals.user.onboarded_at) throw redirect(302, '/home');
+	if (locals.user.onboarded_at && !isAdmin(locals.user.hca_id)) throw redirect(302, '/home');
 	return { user: locals.user };
 };
